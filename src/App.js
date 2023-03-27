49472ba96af2754389e6db200bccfb02a716ac9d
alexiscyber14 <142sledgehammer14@gmail.com>
2023-03-27 23:53:38 +0100

    Add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+            <div className="list-wrapper">
+        <div className="form-cont">
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div className="list-container">
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+      </div>
     </div>
   );
 }
